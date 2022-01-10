16072faaf0475ac6ebd49375b1cf7bdd37e83cb3
Alan Czemerinski <alanczeme@gmail.com>
2022-01-10 13:55:52 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(length){
+  if(length <= 400) {
+    return 'This one is on me!';
+  }
+  else if(length > 2500) {
+    return "No can do.";
+  }
+  else if(length > 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if(city === "NYC") {
+    return "Ok, sounds good.";
+  }
+  else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
